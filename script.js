b72fc0e7f709e8162568fe237ed216fe3fe4c683
Ritik Sharma <ritiksharma10508@gmail.com>
2026-01-29 18:15:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const btn = document.getElementById("button")
+const fntclr = document.getElementById("fontcolor")
+const fntsz = document.getElementByID("fontsize")
+
+btn.addEventListener("click",()=>{
+	document.cookie = "fontcolor": fntclr.value
+	document.cookie = "fontsize": fntsz.value
+})
